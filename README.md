94ab994c4ba7ea64a5a180788304cd5dfe06c0d8
Seliun <seliun@protonmail.com>
2022-12-24 21:16:32 +0300

    Shoo Store

---
+++ README.md
@@ -0,0 +1,19 @@
+# Shoo Store - E-commerce Website
+
+Shoo is a fully responsive landing page for an online shoe store. The code is written purely with HTML, CSS, and JavaScript.
+
+Go to website:
+
+## ✨ How It Looks:
+
+### Desktop ⬇️
+
+![Shoo Store Landing Page](Website-Image/desktop_1.png)
+
+![Shoo Store Landing Page](Website-Image/desktop_2.png)
+
+### Mobile ⬇️
+
+![Shoo Store Landing Page](Website-Image/mobile_1.png)
+
+![Shoo Store Landing Page](Website-Image/mobile_2.png)
---
+++ script.js
@@ -0,0 +1,137 @@
+const wrapper = document.querySelector('.sliderWrapper');
+
+const menuItems = document.querySelectorAll('.menuItem');
+
+const products = [
+  {
+    id: 1,
+    title: 'Air Force',
+    price: 119,
+    colors: [
+      {
+        code: 'black',
+        img: './img/air.png',
+      },
+      {
+        code: 'darkblue',
+        img: './img/air2.png',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Air Jordan',
+    price: 149,
+    colors: [
+      {
+        code: 'lightgray',
+        img: './img/jordan.png',
+      },
+      {
+        code: 'green',
+        img: './img/jordan2.png',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Blazer',
+    price: 109,
+    colors: [
+      {
+        code: 'lightgray',
+        img: './img/blazer.png',
+      },
+      {
+        code: 'green',
+        img: './img/blazer2.png',
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: 'Crater',
+    price: 129,
+    colors: [
+      {
+        code: 'black',
+        img: './img/crater.png',
+      },
+      {
+        code: 'lightgray',
+        img: './img/crater2.png',
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: 'Hippie',
+    price: 99,
+    colors: [
+      {
+        code: 'gray',
+        img: './img/hippie.png',
+      },
+      {
+        code: 'black',
+        img: './img/hippie2.png',
+      },
+    ],
+  },
+];
+
+let chosenProduct = products[0];
+
+const currentProductImg = document.querySelector('.productImage');
+const currentProductTitle = document.querySelector('.productTitle');
+const currentProductPrice = document.querySelector('.productPrice');
+const currentProductColors = document.querySelectorAll('.color');
+const currentProductSizes = document.querySelectorAll('.size');
+
+menuItems.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    //changes the current slide
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+    //changes the chosen product
+    chosenProduct = products[index];
+
+    //changes the texts of current product
+    currentProductTitle.textContent = chosenProduct.title.toUpperCase();
+    currentProductPrice.textContent = '$' + chosenProduct.price;
+    currentProductImg.src = chosenProduct.colors[0].img;
+
+    //changes the style of product by selected color
+    currentProductColors.forEach((color, index) => {
+      color.style.backgroundColor = chosenProduct.colors[index].code;
+    });
+  });
+});
+
+currentProductColors.forEach((color, index) => {
+  color.addEventListener('click', () => {
+    currentProductImg.src = chosenProduct.colors[index].img;
+  });
+});
+
+currentProductSizes.forEach((size, index) => {
+  size.addEventListener('click', () => {
+    currentProductSizes.forEach((size) => {
+      size.style.backgroundColor = 'white';
+      size.style.color = 'black';
+    });
+    size.style.backgroundColor = 'black';
+    size.style.color = 'white';
+  });
+});
+
+const productButton = document.querySelector('.productButton');
+const payment = document.querySelector('.payment');
+const close = document.querySelector('.close');
+
+productButton.addEventListener('click', () => {
+  payment.style.display = 'flex';
+});
+close.addEventListener('click', () => {
+  payment.style.display = 'none';
+});
